ea320629cdbc88d0ddd72e1b62f113cbafa0419f
Mokammel Hossain Tanvir <mokammeltanvir01@gmail.com>
2023-04-09 23:27:08 +0600

    Home Ui design with Swiper

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -2,13 +2,13 @@
   <section id="home" class="grid place-items-center mt-20 md:mt-10 overflow-hidden">
 
   <div class="grid grid-flow-row-dense grid-cols-1 lg:grid-cols-2 items-end gap-10 lg:gap-16 w-full px-0 lg:px-10">
-  <div class="text-5xl md:text-6xl font-bold text-white w-full pb-20 order-2 lg:order-1">
-  <div class="flex items-center gap-3">
-  <h2 class="uppercase">Booo</h2>
+  <div class="text-5xl md:text-6xl text-center md:text-left font-bold text-white w-full pb-20 order-2 lg:order-1">
+  <div class="flex items-center gap-3 ml-24 md:ml-0">
+  <h2 class="uppercase text-center md:text-left">Booo</h2>
   <img src="../assets/img/head1-img.png" alt="head1-img">
   </div>
   <h2 class="text-4xl md:text-5xl uppercase">Trick or Treat</h2>
-  <p class="pt-5 text-lg font-normal">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Enim commodi nostrum assumenda in sed impedit eum dolorum nam voluptatem soluta!</p>
+  <p class="pt-5 text-lg font-normal ">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Enim commodi nostrum assumenda in sed impedit eum dolorum nam voluptatem soluta!</p>
     </div>
 
     <div class="w-full h-full md:pb-20 order-1 lg:order-2">
